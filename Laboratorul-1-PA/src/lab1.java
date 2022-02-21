29e1794bd202a21e523b5471039dfcc9ba56c78c
Manea-Loredana-B1 <mloredana056@gmail.com>
2022-02-21 23:22:55 +0200

    lab1

--- Laboratorul-1-PA/src/lab1.java
+++ Laboratorul-1-PA/src/lab1.java
@@ -8,19 +8,18 @@ public class Lab1 {
     public static void main(String[] args) {
         Lab1 lab1 = new Lab1();
         lab1.compulsory();
-        lab1.homework(args) ;
-        lab1.bonus();
+        lab1.homework(args);
+//        lab1.bonus();
         long startTime = System.nanoTime();
         long endTime = System.nanoTime();
         long totalTime = endTime - startTime;
         System.out.println("Time in nanoseconds: " + totalTime);
 
 
-
     }
 
-     void compulsory() {
-         // Hello world! print
+    void compulsory() {
+        // Hello world! print
         System.out.println("Hello world!");
         //second step
         String[] c = {"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
@@ -34,15 +33,14 @@ public class Lab1 {
         int prod1 = n * 3;
         int sum1 = prod1 + 0b10101;
         int sum2 = sum1 + 0xFF;
-         int b = sum2 * 6;
-        while (b > 9)
-        { int sum =0 ;
-            while (b != 0)
-            {
+        int b = sum2 * 6;
+        while (b > 9) {
+            int sum = 0;
+            while (b != 0) {
                 sum = sum + b % 10;
-                b = b/10;
+                b = b / 10;
             }
-            b=sum;
+            b = sum;
 
         }
 //        Compute the sum of the digits in the result obtained in the previous step. This is the new result.
@@ -70,12 +68,12 @@ public class Lab1 {
         int p = Integer.parseInt(args[1]);
         int m = args.length - 2;
         char[] alphabet = new char[m];
-        for(int i=0; i<m; i++) {
-            alphabet[i] = args[i+2].charAt(0);
+        for (int i = 0; i < m; i++) {
+            alphabet[i] = args[i + 2].charAt(0);
         }
-        String[] c= new String[n];
-        for(int i=0; i<n ;i++){
-            c[i]=app.createRandomWord(p, alphabet);
+        String[] c = new String[n];
+        for (int i = 0; i < n; i++) {
+            c[i] = app.createRandomWord(p, alphabet);
             System.out.println(c[i]);
 
         }
@@ -88,7 +86,7 @@ public class Lab1 {
 //Two words are neighbors if they have a common letter.
 //Create a boolean n x n matrix, representing the adjacency relation of the words.
 
-        if(n < 30000 ) {
+        if (n < 30000) {
             long[][] A = new long[n][n];
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
@@ -103,62 +101,70 @@ public class Lab1 {
 //Display this data structure on the screen.
             System.out.println(Arrays.deepToString(A));
         }
+///BONUS
+        int k=0;
+        String[] w= new String[n];
+
+        for(int i = 0; i < n-3 ;i++) {// n-1  because if k=n
+        if(Objects.equals(Neighbors(c[i], c[i+1]), "YES")){
+            w[i]=c[i];
+
+            k++;
+        }
+
+        if(Objects.equals(w[1], w[k + 1]) && k>=3){
+
+            System.out.println(k);
+
+            break;
+
+        }}
+
+
+
+
 
     }
 
 
     //Create an array of n strings (called words), each word containing
     // exactly p characters from the given alphabet.
-    private String createRandomWord ( int len, char[] alphabet){
-            StringBuilder word = new StringBuilder();
-            Random rand = new Random();
-            for (int i = 0; i < len; i++) {
-                int k = rand.nextInt(alphabet.length);
-                word.append(alphabet[k]);
-            }
-            return word.toString();
+    private String createRandomWord(int len, char[] alphabet) {
+        StringBuilder word = new StringBuilder();
+        Random rand = new Random();
+        for (int i = 0; i < len; i++) {
+            int k = rand.nextInt(alphabet.length);
+            word.append(alphabet[k]);
         }
+        return word.toString();
+    }
 
     //Two words are neighbors if they have a common letter
-    static String  Neighbors(String s1, String s2) {
-        String result="NO";
+    static String Neighbors(String s1, String s2) {
+        String result = "NO";
         Set<Character> set1 = new HashSet<>(); //	It is used to construct a default HashSet
-  //Set contains unique elements only
-        for (char s : s1.toCharArray()){     // converts this string into character array and put them in to a set
+        //Set contains unique elements only
+        for (char s : s1.toCharArray()) {     // converts this string into character array and put them in to a set
             set1.add(s);
         }
 
-        for(int i=0;i<s2.length();i++){
-            if(set1.contains(s2.charAt(i))){ //The contains() method checks whether a string contains a sequence of characters. (return true or false)
+        for (int i = 0; i < s2.length(); i++) {
+            if (set1.contains(s2.charAt(i))) { //The contains() method checks whether a string contains a sequence of characters. (return true or false)
                 result = "YES";
                 break;
             }
         }
         return result;
-    }
-
-
-
-
-
 
 
 //    Implement an efficient algorithm that determines, if possible, a subset of words W1,W2,...,Wk
 //    (from the ones that you have generated) such that k ≥ 3 and Wi and Wi+1 are neighbors, for all
 //    i in [1..k], where Wk+1=W1.
 //    Can you find the largest possible k?
-     void bonus() {
-
-    //it s possible to find the subset
-    // int k=0;
-   //for(int i=0,i<n-1,i++) // n-1  because if k=n
-   // if(if(Objects.equals(Neighbors(w[i], w[i+1]), "YES")){
-   // K++;}
-   //  while (K>=3)
-   //  if(W[1]==W[K+1]
-     //
-
-      }
 
 
+        //it s possible to find the subset
+
+
+    }
 }
