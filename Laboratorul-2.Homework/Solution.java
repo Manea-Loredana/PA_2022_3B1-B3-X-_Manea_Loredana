f916c04513838dc34f1ea170e2c12df1a88261a6
Manea-Loredana-B1 <mloredana056@gmail.com>
2022-03-06 20:29:52 +0200

    lab2 -Homework

--- Laboratorul-2.Homework/Solution.java
+++ Laboratorul-2.Homework/Solution.java
@@ -1,32 +1,26 @@
-
-
 import java.util.Arrays;
 
 public class Solution {
-    private String[] computeUsedRooms;
-    //assignment[i] = what room received the event with index i
 
-    public String[] computeUsedRooms(Problem pb) {
+    //assignment[i] = what room received the event with index i
+    private String[] assignment ;
+     void computeUsedRooms(Problem pb) {
         Event[] event = pb.getEvents();
         Room[] room = pb.getRooms();
-        String[] assignment = new String[event.length];
+        assignment = new String[event.length];
         for (int i = 0; i < event.length; i++)
-            for (int j = 0; j < room.length; j++)
-                if (event[i].getCapacity() == room[j].getCap())
-                    assignment[i] = room[j].name;
+            for (Room value : room)
+                if (event[i].getCapacity() == value.getCap())
+                    assignment[i] = value.name;
 
-        return assignment;
     }
-
 
     @Override
     public String toString() {
         return "Solution{" +
-                "computeUsedRooms=" + Arrays.toString(computeUsedRooms) +
+                "Rooms used for each event in order=" + Arrays.toString(assignment) +
                 '}';
     }
-
-
 }
 
 
